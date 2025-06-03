82bad9c65daab409d07392b0d10e3b9f1951cd66
Zohar Zilcha <101567143+ZoharZil@users.noreply.github.com>
2025-06-03 08:52:54 +0300

    Delete Exercise #1 directory

--- Exercise #1/Question #1.py
+++
@@ -1,6 +0,0 @@
-numbers = int(input("Enter a Number: "))
-divided_num = []
-for i in range(1, numbers+1):
-    if numbers % i == 0:
-        divided_num.append(i)
-print(divided_num)
--- Exercise #1/Question #3.py
+++
@@ -1,8 +0,0 @@
-word_counter = []
-while True:
-    word = input("Please Type a word: ")
-    if word in word_counter:
-        print(f'You entered the word: "{word}" twice. Good bye')
-        break
-    else:
-        word_counter.append(word)
